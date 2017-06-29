3351f9e9919964d4ef4d755505fc3dace2b7dd2c
end <end@3nd.io>
2017-06-29 02:30:23 +0200

    * fixed sdlwrapper

---
+++ sdlwrapper/sdlwindow.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+
+#ifdef USE_GAWO
+#include <gawo/glwindow.hpp>
+#endif
+#include <sdlwrapper/sdlwrapper.hpp>
+
+namespace sdl2 {
+
+/*
+  The window is just a wrapper for managing the SDL window and it's associated OpenGL context.
+
+  important note: it initializes all SDL subsystems (also things like SDL_image/SDL_ttf/etc).
+*/
+class SDLWindow
+#ifdef USE_GAWO
+    : gawo::GLWindow
+#endif
+{
+private:
+protected:
+  bool m_initializedgl = false;
+
+  uint32_t m_width;
+  uint32_t m_height;
+
+  void handleEvent(SDL_Event event);
+
+  sdl2::WindowPtr m_window;
+  SDL_GLContext m_context;
+
+public:
+  SDLWindow();
+  ~SDLWindow();
+  //opening the window and initializing gl
+  void open();
+  /* eventloop */
+  void poll();
+  // buffer swapping
+  void swap();
+
+  /* some getters */
+  uint32_t getWidth();
+  uint32_t getHeight();
+  bool initializedGL();
+  void setTitle(std::string title);
+  std::string getTitle();
+};
+}
---
+++ sdlwrapper/sdlwrapper.hpp
@@ -0,0 +1,47 @@
+/*
+  RAII compatible pointer types for some SDL structs.
+*/
+#pragma once
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_opengl.h>
+#include <SDL2/SDL_ttf.h>
+#include <memory.h>
+
+namespace sdl2 {
+struct SDL_Deleter {
+  void operator()(SDL_Surface* ptr) {
+    if (ptr)
+      SDL_FreeSurface(ptr);
+  }
+  void operator()(SDL_Texture* ptr) {
+    if (ptr)
+      SDL_DestroyTexture(ptr);
+  }
+  void operator()(SDL_Renderer* ptr) {
+    if (ptr)
+      SDL_DestroyRenderer(ptr);
+  }
+  void operator()(SDL_Window* ptr) {
+    if (ptr)
+      SDL_DestroyWindow(ptr);
+  }
+  void operator()(SDL_RWops* ptr) {
+    if (ptr)
+      SDL_RWclose(ptr);
+  }
+  void operator()(TTF_Font* ptr) {
+    if (ptr)
+      TTF_CloseFont(ptr);
+  }
+};
+
+// Unique Pointers
+using SurfacePtr = std::unique_ptr<SDL_Surface, SDL_Deleter>;
+using TexturePtr = std::unique_ptr<SDL_Texture, SDL_Deleter>;
+using RendererPtr = std::unique_ptr<SDL_Renderer, SDL_Deleter>;
+using WindowPtr = std::unique_ptr<SDL_Window, SDL_Deleter>;
+using RWopsPtr = std::unique_ptr<SDL_RWops, SDL_Deleter>;
+using FontPtr = std::unique_ptr<TTF_Font, SDL_Deleter>;
+}
